40cab1ad0c48653f76c6fb957647776bf4aa2599
Victor Butler <victorbutler@gmail.com>
2016-11-19 13:21:24 -0700

    Only power the sensor when you need a reading - to cut down on electrolysis and corrosion on the sensor pads. If you leave it powered on all the time, you can introduce too much acidity in the soil and kill your plants

--- soil-monitor/soil-monitor.ino
+++ soil-monitor/soil-monitor.ino
@@ -1,13 +1,17 @@
 const int sensorPin = A0;
+const int powerPin = 7;
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(powerPin, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(powerPin, HIGH);
   int val = analogRead(sensorPin);
+  digitalWrite(powerPin, LOW);
   Serial.println(val);
   delay(60000);
 }
